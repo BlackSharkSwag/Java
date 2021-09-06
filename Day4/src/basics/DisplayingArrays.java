fd758b83a970e010d76e86011ec261f8760d47c9
Collin Snyder <37968089+BlackSharkSwag@users.noreply.github.com>
2021-09-06 09:49:10 -0600

    Add files via upload

---
+++ Day4/src/basics/DisplayingArrays.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class DisplayingArrays {
+	public static void main(String [] artgs) {
+		int [] intArray = new int[3];
+		intArray[0] = 35;
+		intArray[1] = 20;
+		intArray[2] = 30;
+		
+		System.out.println(intArray[0]);
+		System.out.println(intArray[1]);
+		System.out.println(intArray[2]);
+		
+		 int[] arr = {1,2,3,4,5,6,12,8,9,11};
+		    System.out.println("Array length: " + arr.length);
+		    
+	}
+}
---
+++ Day4/src/basics/ExampleTwo.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class ExampleTwo {
+	public static void main(String[] args) {
+	boolean on = false;
+	
+	do {
+		System.out.println("Inside the do-while loop");
+	} while (on);
+	}
+}
+
